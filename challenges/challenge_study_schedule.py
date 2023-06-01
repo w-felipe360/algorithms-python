6f1413dc4188145e85e2c86ef44a2976b7330b44
Willian Felipe <w.felipebraz@gmail.com>
2023-06-01 17:32:46 -0300

    challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    students = 0
+    try:
+        for schedules in permanence_period:
+            if schedules[0] <= target_time <= schedules[1]:
+                students += 1
+        return students
+    except:
+        return None
+    # raise NotImplementedError
