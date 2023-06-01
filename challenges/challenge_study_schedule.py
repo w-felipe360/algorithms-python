5afc17966187db2a4aa3663e81c29876410c7a67
Willian Felipe <w.felipebraz@gmail.com>
2023-06-01 17:36:25 -0300

    fix lint study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,11 +3,12 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     students = 0
-    try:
-        for schedules in permanence_period:
-            if schedules[0] <= target_time <= schedules[1]:
-                students += 1
-        return students
-    except:
-        return None
+    for schedules in permanence_period:
+        if len(schedules) != 2 or any(
+            not isinstance(isNum, int) for isNum in schedules
+        ):
+            return None
+        if schedules[0] <= target_time <= schedules[1]:
+            students += 1
+    return students
     # raise NotImplementedError
